15a45dfb2fcf9cc04b360b49e4a50d05cf313097
Juneki Hong <junekihong@gmail.com>
2015-02-08 16:58:48 -0500

    EM.sh is a script that does a quick parameter sweep on the number of EM iterations.

---
+++ hw1/EM.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+rm -f results/EM_*
+rm -f outputs/output_*
+
+rm -f AER.txt
+touch AER.txt
+
+for i in `seq 1 5`;
+do
+    echo "time ./align -n 1000 -i $i > output.txt && cat output.txt | ./check | ./grade > results/EM_$i.txt" 
+    time ./align -n 1000 -i $i > output.txt && cat output.txt | ./check | ./grade > results/EM_$i.txt
+    
+    echo "cp output.txt outputs/output_$i.txt"
+    cp output.txt outputs/output_$i.txt
+
+    tail --lines 3 results/EM_$i.txt
+
+    tail --lines 1 results/EM_$i.txt >> AER.txt
+
+    echo
+done
+
+
+tail --lines 3 results/*
+
+
